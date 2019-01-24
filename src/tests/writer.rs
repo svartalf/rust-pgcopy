b5f21e3f796dfe6da454263b2f92dad80e84d73c
svartalf <self@svartalf.info>
2019-01-24 22:49:38 +0300

    UUID type

--- src/tests/writer.rs
+++ src/tests/writer.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+
+use uuid::Uuid;
+
 use crate::Writer;
 
 #[test]
@@ -46,3 +50,33 @@ fn test_bool() {
 
     assert_eq!(&expected, writer.inner());
 }
+
+#[test]
+fn test_uuid() {
+    let buf: Vec<u8> = vec![];
+    let mut writer = Writer::new(buf);
+    assert!(writer.write_header().is_ok());
+
+    assert!(writer.write_tuple(2).is_ok());
+    assert!(writer.write_uuid(Uuid::from_str("1d662762-2010-11e9-ad8b-c869cdb5cd46").unwrap()).is_ok());
+    assert!(writer.write_uuid(Uuid::from_str("e11b8974-5245-4eca-a06b-a9a440074131").unwrap()).is_ok());
+
+    assert!(writer.write_trailer().is_ok());
+
+    let expected = vec![
+        0x50u8, 0x47, 0x43, 0x4f, 0x50, 0x59, 0x0a, 0xff, 0x0d, 0x0a, 0x00, // PGCOPY\n\xff\r\n\0
+        0x00, 0x00, 0x00, 0x00, // flags
+        0x00, 0x00, 0x00, 0x00, //extension area length
+
+        // One tuple
+        0x00, 0x02, // fields count
+        0x00, 0x00, 0x00, 0x10, 0x1d, 0x66, 0x27, 0x62, 0x20, 0x10, 0x11, 0xe9, 0xad, 0x8b, 0xc8, 0x69, 0xcd, 0xb5, 0xcd, 0x46,
+        0x00, 0x00, 0x00, 0x10, 0xe1, 0x1b, 0x89, 0x74, 0x52, 0x45, 0x4e, 0xca, 0xa0, 0x6b, 0xa9, 0xa4, 0x40, 0x07, 0x41, 0x31,
+
+        0xff, 0xff // trailer
+    ];
+
+    assert_eq!(&expected, writer.inner());
+}
+
+
--- src/writer.rs
+++ src/writer.rs
@@ -1,6 +1,7 @@
 use std::io::{Write, Result};
 
 use byteorder::{WriteBytesExt, BigEndian};
+use uuid::Uuid;
 
 #[derive(Debug)]
 pub struct Writer<W: Write> {
@@ -37,6 +38,8 @@ impl<W: Write> Writer<W> {
     }
 
     // Integer types
+    // https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/int.c
+    // https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/int8.c
 
     pub fn write_i16(&mut self, value: i16) -> Result<()> {
         self.inner.write_i32::<BigEndian>(2)?;
@@ -69,7 +72,9 @@ impl<W: Write> Writer<W> {
     }
 
     // Arbitrary precision numbers
+
     // Floating point types
+    // https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/float.c
 
     pub fn write_f32(&mut self, value: f32) -> Result<()> {
         self.inner.write_i32::<BigEndian>(4)?;
@@ -85,17 +90,28 @@ impl<W: Write> Writer<W> {
     // Character types
     // Binary data types
     // Date/time types
+
     // Boolean type
+    // https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/bool.c
 
     pub fn write_bool(&mut self, value: bool) -> Result<()> {
         self.inner.write_i32::<BigEndian>(1)?;
         self.inner.write_i8(value as i8)
     }
+
     // Enumerated types
     // Geometric types
     // Network address types
     // Bit String types
+
     // UUID type
+    pub fn write_uuid(&mut self, value: Uuid) -> Result<()> {
+        self.inner.write_i32::<BigEndian>(16)?;
+        self.inner.write(value.as_bytes())?;
+
+        Ok(())
+
+    }
     // XML type
     // JSON types
     // Arrays
