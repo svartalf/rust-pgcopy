a521a30e9e5e77457f74c2f62259013f6b4076a1
svartalf <self@svartalf.info>
2019-01-27 16:43:17 +0300

    Replacing BigEndian with a NetworkEndian

--- src/writer.rs
+++ src/writer.rs
@@ -1,7 +1,7 @@
 use std::i32;
 use std::io::{Write, Result};
 
-use byteorder::{WriteBytesExt, BigEndian};
+use byteorder::{WriteBytesExt, NetworkEndian};
 use chrono::{NaiveDateTime, DateTime, TimeZone, Datelike, Timelike};
 use uuid::Uuid;
 
@@ -24,24 +24,24 @@ impl<W: Write> Writer<W> {
 
     pub fn write_header(&mut self) -> Result<()> {
         self.inner.write_all(b"PGCOPY\n\xff\r\n\0")?;
-        self.inner.write_i32::<BigEndian>(0)?;  // flags, empty for now
-        self.inner.write_i32::<BigEndian>(0)?;  // extension area length
+        self.inner.write_i32::<NetworkEndian>(0)?;  // flags, empty for now
+        self.inner.write_i32::<NetworkEndian>(0)?;  // extension area length
 
         Ok(())
     }
 
     pub fn write_trailer(&mut self) -> Result<()> {
-        self.inner.write_i16::<BigEndian>(-1)
+        self.inner.write_i16::<NetworkEndian>(-1)
     }
 
     // Each tuple begins with a 16-bit integer count of the number of fields in the tuple.
     // (Presently, all tuples in a table will have the same count, but that might not always be true.)
     pub fn write_tuple(&mut self, fields: i16) -> Result<()> {
-        self.inner.write_i16::<BigEndian>(fields)
+        self.inner.write_i16::<NetworkEndian>(fields)
     }
 
     pub fn write_null(&mut self) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(-1)
+        self.inner.write_i32::<NetworkEndian>(-1)
     }
 
     // Integer types
@@ -49,33 +49,33 @@ impl<W: Write> Writer<W> {
     // https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/int8.c
 
     pub fn write_i16(&mut self, value: i16) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(2)?;
-        self.inner.write_i16::<BigEndian>(value)
+        self.inner.write_i32::<NetworkEndian>(2)?;
+        self.inner.write_i16::<NetworkEndian>(value)
     }
 
     pub fn write_i32(&mut self, value: i32) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(4)?;
-        self.inner.write_i32::<BigEndian>(value)
+        self.inner.write_i32::<NetworkEndian>(4)?;
+        self.inner.write_i32::<NetworkEndian>(value)
     }
 
     pub fn write_i64(&mut self, value: i64) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(8)?;
-        self.inner.write_i64::<BigEndian>(value)
+        self.inner.write_i32::<NetworkEndian>(8)?;
+        self.inner.write_i64::<NetworkEndian>(value)
     }
 
     pub fn write_u16(&mut self, value: u16) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(2)?;
-        self.inner.write_u16::<BigEndian>(value)
+        self.inner.write_i32::<NetworkEndian>(2)?;
+        self.inner.write_u16::<NetworkEndian>(value)
     }
 
     pub fn write_u32(&mut self, value: u32) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(4)?;
-        self.inner.write_u32::<BigEndian>(value)
+        self.inner.write_i32::<NetworkEndian>(4)?;
+        self.inner.write_u32::<NetworkEndian>(value)
     }
 
     pub fn write_u64(&mut self, value: u64) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(8)?;
-        self.inner.write_u64::<BigEndian>(value)
+        self.inner.write_i32::<NetworkEndian>(8)?;
+        self.inner.write_u64::<NetworkEndian>(value)
     }
 
     // TODO: Arbitrary precision numbers
@@ -84,13 +84,13 @@ impl<W: Write> Writer<W> {
     // https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/float.c
 
     pub fn write_f32(&mut self, value: f32) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(4)?;
-        self.inner.write_f32::<BigEndian>(value)
+        self.inner.write_i32::<NetworkEndian>(4)?;
+        self.inner.write_f32::<NetworkEndian>(value)
     }
 
     pub fn write_f64(&mut self, value: f64) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(8)?;
-        self.inner.write_f64::<BigEndian>(value)
+        self.inner.write_i32::<NetworkEndian>(8)?;
+        self.inner.write_f64::<NetworkEndian>(value)
     }
 
     // TODO: Serial types
@@ -101,7 +101,7 @@ impl<W: Write> Writer<W> {
         let str = value.as_ref();
         debug_assert!(str.len() < i32::MAX as usize);
 
-        self.inner.write_i32::<BigEndian>(str.len() as i32)?;  // TODO: Possible value truncation
+        self.inner.write_i32::<NetworkEndian>(str.len() as i32)?;  // TODO: Possible value truncation
         self.inner.write_all(str.as_bytes())?;
 
         Ok(())
@@ -112,7 +112,7 @@ impl<W: Write> Writer<W> {
         let bytes = value.as_ref();
         debug_assert!(bytes.len() < i32::MAX as usize);
 
-        self.inner.write_i32::<BigEndian>(bytes.len() as i32)?;
+        self.inner.write_i32::<NetworkEndian>(bytes.len() as i32)?;
         self.inner.write_all(bytes)?;
 
         Ok(())
@@ -126,8 +126,8 @@ impl<W: Write> Writer<W> {
         // This big number is a microseconds amount between UNIX epoch and PSQL epoch
         let us = (value.timestamp_nanos() / 1_000) - 946_684_800_000_000;
 
-        self.inner.write_i32::<BigEndian>(8)?;
-        self.inner.write_i64::<BigEndian>(us)
+        self.inner.write_i32::<NetworkEndian>(8)?;
+        self.inner.write_i64::<NetworkEndian>(us)
     }
 
     // Date and time (with time zone)
@@ -139,22 +139,22 @@ impl<W: Write> Writer<W> {
         // 730_120 is a days amount from the "Day 1" to PSQL epoch date (2000-01-01)
         let days = value.num_days_from_ce() - 730_120;
 
-        self.inner.write_i32::<BigEndian>(4)?;
-        self.inner.write_i32::<BigEndian>(days)
+        self.inner.write_i32::<NetworkEndian>(4)?;
+        self.inner.write_i32::<NetworkEndian>(days)
     }
 
     pub fn write_time<T: Timelike>(&mut self, value: T) -> Result<()> {
         let us = i64::from(value.num_seconds_from_midnight()) * 1_000 * 1_000 + i64::from(value.nanosecond() / 1_000);
 
-        self.inner.write_i32::<BigEndian>(8)?;
-        self.inner.write_i64::<BigEndian>(us)
+        self.inner.write_i32::<NetworkEndian>(8)?;
+        self.inner.write_i64::<NetworkEndian>(us)
     }
 
     // Boolean type
     // https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/bool.c
 
     pub fn write_bool(&mut self, value: bool) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(1)?;
+        self.inner.write_i32::<NetworkEndian>(1)?;
         self.inner.write_i8(value as i8)
     }
 
@@ -165,7 +165,7 @@ impl<W: Write> Writer<W> {
 
     // UUID type
     pub fn write_uuid(&mut self, value: Uuid) -> Result<()> {
-        self.inner.write_i32::<BigEndian>(16)?;
+        self.inner.write_i32::<NetworkEndian>(16)?;
         self.inner.write_all(value.as_bytes())?;
 
         Ok(())
